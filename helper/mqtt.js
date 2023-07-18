dff081a615c4bfdee03cf826c4684d35c07c7cea
Rizky Ramadhani <83220358+rizkiyee@users.noreply.github.com>
2023-07-18 23:45:15 +0700

    "tambah timestamp data yang dikirim ke mqtt"

--- helper/mqtt.js
+++ helper/mqtt.js
@@ -2,8 +2,6 @@ const mqtt = require('mqtt');
 const ruleController = require('../src/controller/rule.controller');
 const ruleModel = require('../src/model/rule.model');
 
-
-
 const host = 'test.mosquitto.org'
 const port = '1883'
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
@@ -19,40 +17,30 @@ const client = mqtt.connect(connectUrl, {
 })
 
 
-let triggerRule;
-let serviceRule;
 let rule;
+
+const getTime = () => {
+    let date = new Date();
+    let
+        hour = date.getHours(),
+        minute = date.getMinutes(),
+        second = date.getSeconds(),
+        milisecond = date.getMilliseconds();
+    let timeString = hour + ':' + minute + ':' + second + ':' + milisecond + ' -->';
+    return timeString;
+}
+
+let currentTime = getTime();
+// console.log(getTimestampInSeconds());
 client.on('connect', () => {
     ruleModel.get().then(result => {
-        console.log('Connected')
-        // console.log(ruleController.trigger);
-        // console.log(result);
-
+        console.log('MQTT Connected')
         result.forEach(value => {
-            // console.log(topic);
-            triggerRule = 'trigger_id: ' + value.trigger_id + ' trigger_val : ' + value.trigger_val;
-            serviceRule = 'service_id: ' + value.service_id + ' service_val : ' + value.service_val;
-            rule = `{"id: "${value.id}", "trigger_id" : "${value.trigger_id}", "trigger_val" : "${value.trigger_val}", 
+            rule = `{"trigger_id" : "${value.trigger_id}", "trigger_val" : "${value.trigger_val}", 
                     "service_id" : "${value.service_id}", "service_val" : "${value.service_val}"}`
-            client.subscribe('trigger', () => {
-                // console.log(`Subscribe to topic '${topic}'`);
-            })
-            client.subscribe('service', () => {
-
-            })
             client.subscribe('rule', () => {
 
             })
-            // client.publish('trigger', 'rule trigger: ' + triggerRule, { qos: 0, retain: false }, (error) => {
-            //     if (error) {
-            //         console.error(error);
-            //     }
-            // })
-            // client.publish('service', 'rule service: ' + serviceRule, { qos: 0, retain: false }, (error) => {
-            //     if (error) {
-            //         console.log(error);
-            //     }
-            // })
             client.publish('rule', rule, { qos: 0, retain: false }, (error) => {
                 if (error) {
                     console.log(error);
@@ -60,10 +48,9 @@ client.on('connect', () => {
             })
         })
         client.on('message', (topic, payload) => {
-            console.log('Received Message:', payload.toString())
+            console.log(currentTime, payload.toString())
         })
     })
 })
-
 
 module.exports = mqtt;
